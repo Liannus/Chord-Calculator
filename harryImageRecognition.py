d76f8db15420921ae73f00a4807e78acdb7a6f06
Harry <harry_ism@hotmail.com>
2020-03-21 17:10:13 -0400

    Set up basic image recognition.

---
+++ harryImageRecognition.py
@@ -0,0 +1,15 @@
+from imageai.Prediction.Custom import CustomImagePrediction
+import os
+
+execution_path = os.getcwd()
+
+prediction = CustomImagePrediction()
+prediction.setModelTypeAsResNet()
+prediction.setModelPath("media/images/models/model_ex-001_acc-0.090909.h5")
+prediction.setJsonPath("media/images/json/model_class.json")
+prediction.loadModel(num_objects=11)
+
+predictions, probabilities = prediction.predictImage("media/images/test/b/b.png", result_count=10)
+
+for eachPrediction, eachProbability in zip(predictions, probabilities):
+    print(eachPrediction , " : " , eachProbability)
